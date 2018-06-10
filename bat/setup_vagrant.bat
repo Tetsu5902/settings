9a39974d9ea25a95752b0eb6b02aa562dededc7f
Tetsuro Okuyama <tetsuro.okuyama@gmail.com>
2018-06-10 15:18:56 +0900

    Add vagrant settings

--- bat/setup_vagrant.bat
+++ bat/setup_vagrant.bat
@@ -1,1 +1,8 @@
 vagrant plugin install vagrant-winnfsd
+
+set VAGRANT_INSTALL_DIR="C:\vagrant"
+mklink /D %VAGRANT_INSTALL_DIR% %~dp0app\vagrant
+pushd %VAGRANT_INSTALL_DIR%
+
+vagrant init
+vagrant up
--- setup_windows.bat
+++ setup_windows.bat
@@ -1,13 +1,18 @@
 @echo off
 
-rem Install chocolatey
+rem install chocolatey
 @powershell -NoProfile -ExecutionPolicy RemoteSigned -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-rem Install softwares
-cinst -y conf/chocolatey/init.config
 
-rem Install additional python libs
-bat/setup_anaconda.bat
+rem install softwares
+rem choco config set proxy "URL":"port"
+cinst -y conf/chocolatey/cinst_base.config
+rem cinst -y conf/chocolatey/cinst_ext.config
+
+
+rem install additional python libs
+call bat/setup_anaconda.bat
+
 
 rem install vagrant plugins
-bat/setup_vagrant.bat
+call bat/setup_vagrant.bat
