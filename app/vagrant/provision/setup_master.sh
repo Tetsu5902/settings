25f3098810d10169a37b987c1324684eb6f3c85e
Tetsuro Okuyama <tetsuro.okuyama@gmail.com>
2018-06-16 16:55:06 +0900

    Change VM run order

--- app/vagrant/provision/setup_master.sh
+++ app/vagrant/provision/setup_master.sh
@@ -1,6 +1,11 @@
-#Copy slave ssh keys
-cp -p /share/private_key.slave00 ~/.ssh/
-chmod 700 ~/.ssh/private_key.slave00
+#Copy slave ssh keys(root acount)
+cp -p /share/private_key.slave00 /home/vagrant/.ssh/
+chown vagrant:vagrant /home/vagrant/.ssh/private_key.slave00
+chmod 600 /home/vagrant/.ssh/private_key.slave00
+
+cp -p /share/sshd_config /home/vagrant/.ssh/config
+chown vagrant:vagrant /home/vagrant/.ssh/config
+chmod 600 /home/vagrant/.ssh/config
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
@@ -12,4 +17,4 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get install python3-pip -y
 sudo pip3 install ansible
 
-
+sudo apt-get install emacs25-nox -y
--- bat/setup_vagrant.bat
+++ bat/setup_vagrant.bat
@@ -5,8 +5,10 @@ mklink /D %VAGRANT_INSTALL_DIR% %~dp0app\vagrant
 pushd %VAGRANT_INSTALL_DIR%
 
 vagrant init
+vagrant up ansible-slave00
 vagrant ssh-config ansible-slave00
-copy .vagrant/machines/ansible-slave00/virtualbox/private_key share/private_key.slave00
-vagrant up
+copy ".\\.vagrant\\machines\\ansible-slave00\\virtualbox\\private_key" ".\\share\\private_key.slave00"
+
+vagrant up ansible-master
 
 popd
