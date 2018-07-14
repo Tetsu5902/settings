d4f30a297ca7e7fee6ea0cc040612e8c389afbc2
Tetsuro Okuyama <tetsuro.okuyama@gmail.com>
2018-07-14 19:19:17 +0900

    Fix separate symbol

--- bat/setup_vagrant.bat
+++ bat/setup_vagrant.bat
@@ -1,16 +1,16 @@
 vagrant plugin install vagrant-winnfsd
 
-set VAGRANT_INSTALL_DIR="C:\vagrant"
-mklink /D %VAGRANT_INSTALL_DIR% %~dp0app\vagrant
+set VAGRANT_INSTALL_DIR="C:\\vagrant"
+mklink /D %VAGRANT_INSTALL_DIR% %~dp0app\\vagrant
 pushd %VAGRANT_INSTALL_DIR%
 
 vagrant init
 vagrant up ansible-slave00
 vagrant ssh-config ansible-slave00
-copy ".\\.vagrant\\machines\\ansible-slave00\\virtualbox\\private_key" ".\\share\\ssh\\private_key.slave00"
+copy .\\.vagrant\\machines\\ansible-slave00\\virtualbox\\private_key .\\share\\ssh\\private_key.slave00
 
 vagrant up core-01
-copy %USERPROFILE%\.vagrant.d\insecure_private_key .\share\ssh\private_key.coreos
+copy %USERPROFILE%\\.vagrant.d\\insecure_private_key .\\share\\ssh\\private_key.coreos
 
 vagrant up ansible-master
 
