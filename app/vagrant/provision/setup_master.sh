487bd23e13ad6370e78ba30036da2c418692d671
Tetsuro Okuyama <tetsuro.okuyama@gmail.com>
2018-07-14 11:09:00 +0900

    Use ActivePython for ansible

--- app/vagrant/provision/setup_master.sh
+++ app/vagrant/provision/setup_master.sh
@@ -1,3 +1,6 @@
+PYTHON_TAR_NAME=ActivePython-3.6.0.3600-linux-x86_64-glibc-2.3.6-401834
+
+
 #Copy slave ssh keys(root acount)
 cp -p /share/ssh/private_key.slave00 /home/vagrant/.ssh/
 chown vagrant:vagrant /home/vagrant/.ssh/private_key.slave00
@@ -23,3 +26,8 @@ sudo apt-get install python3-pip -y
 sudo pip3 install ansible
 
 sudo apt-get install emacs25-nox -y
+
+
+scp -i .ssh/private_key.coreos /share/${PYTHON_TAR_NAME}.tar.gz core@172.17.8.101:/home/core/
+ssh core@172.17.8.101 -i .ssh/private_key.coreos tar -xzvf ${PYTHON_TAR_NAME}.tar.gz
+ssh core@172.17.8.101 -i .ssh/private_key.coreos cp -rp ${PYTHON_TAR_NAME}/INSTALLDIR ./python
